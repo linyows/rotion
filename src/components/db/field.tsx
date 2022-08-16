e65038bd80ef418ff7d84134644b33a48608a50e
linyows <linyows@gmail.com>
2022-08-16 11:07:29 +0900

    split field to a component

---
+++ src/components/db/field.tsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import type {
+  GetPagePropertyResponse,
+  RichTextItemResponse,
+  TitlePropertyItemObjectResponse,
+} from '../../types'
+
+import DBTitleField from './title'
+import DBDateField from './date'
+import DBRichTextField from './richtext'
+import DBMultiSelectField from './multiselect'
+import DBUrlField from './url'
+import DBCheckboxField from './checkbox'
+
+export type DBFieldProps = {
+  name: string
+  items: GetPagePropertyResponse|undefined
+  path: string
+  slug: string
+}
+
+export const DBField = ({ name, items, path, slug }: DBFieldProps) => {
+  if (items === undefined) {
+    return <></>
+  }
+
+  if (items.object === 'list') {
+    const target = items.results[0]
+    switch (target.type) {
+      case 'title':
+        const payload = items.results as Array<TitlePropertyItemObjectResponse>
+        return DBTitleField({ payload, path, slug })
+        break
+
+      case 'rich_text':
+      case 'people':
+      case 'relation':
+      case 'rollup':
+      default:
+        console.log('unsupport database property:', target)
+        break
+    }
+  } else {
+    switch (items.type) {
+      case 'date':
+        return DBDateField({ payload: items.date })
+        break
+
+      case 'rich_text':
+        return DBRichTextField({ payload: items })
+        break
+
+      case 'multi_select':
+        return DBMultiSelectField({ payload: items, path })
+        break
+
+      case 'url':
+        return DBUrlField({ payload: items.url })
+        break
+
+      case 'checkbox':
+        return DBCheckboxField({ payload: items.checkbox })
+        break
+
+      case 'number':
+      case 'select':
+      case 'status':
+      case 'email':
+      case 'phone_number':
+      case 'checkbox':
+      case 'files':
+      case 'created_by':
+      case 'created_time':
+      case 'last_edited_by':
+      case 'last_edited_time':
+      case 'formula':
+      case 'people':
+      case 'relation':
+      case 'rollup':
+      default:
+        console.log('unsupport database property:', items)
+        break
+    }
+  }
+}
+
+export default DBField
