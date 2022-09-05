3a62ad45a87dc7ff79482ea222f5dc816786ce08
linyows <linyows@gmail.com>
2022-09-06 00:48:25 +0900

    fix twitter script for embed

--- src/components/page/embed.tsx
+++ src/components/page/embed.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TextBlock from './text'
 import type {
   EmbedBlockObjectResponseEx,
@@ -8,10 +8,35 @@ export type EmbedBlockProps = {
   block: EmbedBlockObjectResponseEx
 }
 
+const TwitterBlock: React.FC<EmbedBlockProps> = ({ block }) => {
+  const htmlWithRemovedScript = block.embed.html.replace(/<script>.*/, '')
+
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://platform.twitter.com/widgets.js'
+    document.getElementsByClassName('notionate-blocks-embed-inner')[0].appendChild(script)
+  }, [])
+
+  return (
+    <div className="notionate-blocks-embed notionate-blocks-embed-twitter">
+      <div className="notionate-blocks-embed-inner" dangerouslySetInnerHTML={{ __html: htmlWithRemovedScript }} />
+      <div className="notionate-blocks-embed-caption">
+        <TextBlock tag="span" block={block.embed.caption} />
+      </div>
+    </div>
+  )
+}
+
 const EmbedBlock: React.FC<EmbedBlockProps> = ({ block }) => {
   if (block.embed?.html === undefined) {
     console.log('The html property for this embed block was undefined:', block)
     return <></>
+  }
+
+  if (block.embed.html.includes('twitter')) {
+    return (
+      <TwitterBlock block={block} />
+    )
   }
 
   return (
