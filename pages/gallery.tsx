c006a0446528e736e068ee68012bb64bfc752ef9
Tomohisa Oda <linyows@gmail.com>
2022-12-25 22:29:09 +0900

    Update gallery.tsx

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -75,7 +75,7 @@ const GalleryPage: NextPage<Props> = ({ title, icon, image, blocks, db }) => {
   return (
     <>
       <Head>
-        <title>List - Notionate</title>
+        <title>Gallery - Notionate</title>
         <link rel="icon" href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${icon}</text></svg>`} />
       </Head>
 
