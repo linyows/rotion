1fe632dffc698bf9a577e0b3dee66cab10ad25ac
linyows <linyows@gmail.com>
2023-04-11 10:42:34 +0900

    fix lint

--- src/components/page/code.tsx
+++ src/components/page/code.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import Prism from 'prismjs'
 import mermaid from 'mermaid'
 import { TextObject } from './text'
