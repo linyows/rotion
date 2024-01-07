18972153ee59bc3e976d3ea68635aa0d0661a67c
linyows <linyows@gmail.com>
2024-01-07 17:58:29 +0900

    change type to interface

--- packages/notionate-ui/src/components/types.ts
+++ packages/notionate-ui/src/components/types.ts
@@ -1,11 +1,11 @@
 import React from 'react'
 import type { UrlObject } from 'node:url'
 
-export type Link = React.FC<{
+export interface Link extends React.FC<{
   children: string | React.ReactElement<'a', string | React.JSXElementConstructor<any>>
   className?: string
   href: string | UrlObject
-}>
+}> {}
 
 export interface ExternalModules {
   mermaid?: {
