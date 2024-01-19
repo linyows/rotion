0bbdf65c85ba4e6dc7996194b728811ee23eff4e
linyows <linyows@gmail.com>
2024-01-19 23:00:38 +0900

    update

---
+++ rollup.config.ts
@@ -0,0 +1,58 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json'
+// import terser from '@rollup/plugin-terser'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import del from 'rollup-plugin-delete'
+import dts from 'rollup-plugin-dts'
+
+export default [
+  {
+    input: './src/ui/index.ts',
+    output: [
+      {
+        intro: "'use client';",
+        file: './dist/ui/cjs/index.js',
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        intro: "'use client';",
+        file: './dist/ui/esm/index.js',
+        format: 'esm',
+        sourcemap: true
+      },
+      {
+        intro: "'use client';",
+        file: './dist/ui/umd/index.js',
+        format: 'umd',
+        name: 'Notionate UI',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      del({ targets: './dist/ui/*' }),
+      peerDepsExternal(),
+      resolve(),
+      json(),
+      commonjs(),
+      typescript(),
+      // terser(),
+    ],
+    external: [
+      'react',
+      'react-dom',
+    ],
+  },
+  {
+    input: './src/ui/index.ts',
+    output: [{
+      file: './dist/ui/types.d.ts',
+      format: 'esm',
+    }],
+    plugins: [
+      dts(),
+    ],
+  },
+]
