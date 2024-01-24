39cc492ac365bb678406b81a9e03895d86017053
linyows <linyows@gmail.com>
2024-01-24 11:06:34 +0900

    root is slash when href is slash in breadcrumbs

--- src/ui/components/Page/BreadcrumbBlock/BreadcrumbBlock.tsx
+++ src/ui/components/Page/BreadcrumbBlock/BreadcrumbBlock.tsx
@@ -3,15 +3,31 @@ import { getLinkPathAndLinkKey, queryToString } from '../../lib'
 import type { Breadcrumb } from '../../../../exporter'
 import type { BreadcrumbBlockProps, BreadcrumbLinkProps } from './BreadcrumbBlock.types'
 
+function buildPathname(id: string, name: string, href?: string) {
+  if (href === '/') {
+    return href
+  }
+
+  const [path, slugKey] = getLinkPathAndLinkKey(href || '')
+  let file = ''
+
+  if (slugKey === 'id') {
+    file = id
+  } else {
+    file = encodeURIComponent(name.toLowerCase()).replace(/%20/g, '-')
+  }
+
+  return `${path}${file}`
+}
+
 const BreadcrumbLink = ({ breadcrumb, link, href, query, children }: BreadcrumbLinkProps) => {
   const { id, name } = breadcrumb
-  const [path, slugKey] = getLinkPathAndLinkKey(href || '')
-  const file = slugKey === 'id' ? id : encodeURIComponent(name.toLowerCase()).replace(/%20/g, '-')
+  const pathname = buildPathname(id, name, href)
 
   if (link && href) {
     const Link = link
     return (
-      <Link className="notionate-blocks-breadcrumb-a" href={{ pathname: `${path}${file}`, query }}>
+      <Link className="notionate-blocks-breadcrumb-a" href={{ pathname, query }}>
         {children}
       </Link>
     )
@@ -19,7 +35,7 @@ const BreadcrumbLink = ({ breadcrumb, link, href, query, children }: BreadcrumbL
 
   if (href) {
     return (
-      <a className="notionate-blocks-breadcrumb-a" href={`${path}${file}${queryToString(query)}`}>
+      <a className="notionate-blocks-breadcrumb-a" href={`${pathname}${queryToString(query)}`}>
         {children}
       </a>
     )
@@ -37,14 +53,16 @@ const BreadcrumbBlock = ({ block, link, hrefs, query }: BreadcrumbBlockProps) =>
   return (
     <div className="notionate-blocks-breadcrumb">
       {block.list.map((v: Breadcrumb, i: number) => (
-        <BreadcrumbLink key={`crumb-${i}`} breadcrumb={v} href={hrefs === undefined ? undefined : hrefs[i]} link={link} query={query}>
-          {v.icon.type === 'emoji' && <span className="notionate-blocks-breadcrumb-emoji">{v.icon.emoji}</span>}
-          {v.icon.type !== 'emoji' && <img className="notionate-blocks-breadcrumb-icon" src={v.icon.src} width={20} height={20} alt={v.name} />}
-          <span className="notionate-blocks-breadcrumb-title">
-            {v.name}
-          </span>
+        <>
+          <BreadcrumbLink key={`crumb-${i}`} breadcrumb={v} href={hrefs === undefined ? undefined : hrefs[i]} link={link} query={query}>
+            {v.icon.type === 'emoji' && <span className="notionate-blocks-breadcrumb-emoji">{v.icon.emoji}</span>}
+            {v.icon.type !== 'emoji' && <img className="notionate-blocks-breadcrumb-icon" src={v.icon.src} width={20} height={20} alt={v.name} />}
+            <span className="notionate-blocks-breadcrumb-title">
+              {v.name}
+            </span>
+          </BreadcrumbLink>
           {i + 1 < max && <span className="notionate-blocks-breadcrumb-slash">/</span>}
-        </BreadcrumbLink>
+        </>
       ))}
     </div>
   )
