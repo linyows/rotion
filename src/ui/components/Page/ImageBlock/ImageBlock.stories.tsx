e5db14e1807d8d352f3a5fb084f2666b647e5dca
linyows <linyows@gmail.com>
2024-02-03 22:10:13 +0900

    change path

--- src/ui/components/Page/ImageBlock/ImageBlock.stories.tsx
+++ src/ui/components/Page/ImageBlock/ImageBlock.stories.tsx
@@ -28,7 +28,7 @@ const block: ImageBlockObjectResponseEx = {
       url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2346&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       expiry_time: '',
     },
-    src: '/misc/beach.jpg',
+    src: '/images/beach.jpg',
     caption: [
       {
         type: 'text',
