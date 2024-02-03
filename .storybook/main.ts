53762cae34f7743309da4af877121978b9ea66ca
linyows <linyows@gmail.com>
2024-02-03 22:02:51 +0900

    add .storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,35 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import { stylexPlugin } from "vite-plugin-stylex-dev";
+
+const config: StorybookConfig = {
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  async viteFinal(config) {
+    const { plugins = [] } = config;
+    plugins.push(
+      stylexPlugin({
+        unstable_moduleResolution: {
+          type: 'commonJS',
+          rootDir: process.cwd()
+        }
+      })
+    )
+    return config
+  },
+};
+export default config;
