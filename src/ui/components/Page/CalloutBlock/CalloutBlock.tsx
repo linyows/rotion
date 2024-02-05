2a9ede52ab7dca8ca9c43a48bb577dc8a55bb711
linyows <linyows@gmail.com>
2024-02-05 22:59:12 +0900

    support full color for callout

--- src/ui/components/Page/CalloutBlock/CalloutBlock.tsx
+++ src/ui/components/Page/CalloutBlock/CalloutBlock.tsx
@@ -5,14 +5,13 @@ import Stylex from '@stylexjs/stylex'
 import { fontFamily } from '../../tokens.stylex'
 
 const style = Stylex.create({
-  callout: {
+  wrapper: {
     fontFamily: fontFamily.sansserif,
     display: 'flex',
     padding: '1.2rem',
     paddingLeft: '1rem',
     width: 'calc(100% - 2.4rem)',
-    borderRadius: '3px',
-    backgroundColor: 'rgb(241, 241, 239)',
+    borderRadius: '4px',
     marginTop: '.6rem',
   },
   icon: {
@@ -21,11 +20,84 @@ const style = Stylex.create({
   },
   text: {
     marginLeft: '.6rem',
+    marginTop: '.15rem',
+  },
+  default: {
+    color: 'rgb(55, 53, 47)',
+    backgroundColor: 'rgb(241, 241, 239)',
+  },
+  default_background: {
+    color: 'rgb(55, 53, 47)',
+    border: '1px solid rgba(55, 53, 47, 0.16)',
+  },
+  gray: {
+    color: 'rgb(120, 119, 116)',
+    border: '1px solid rgba(55, 53, 47, 0.16)',
+  },
+  gray_background: {
+    backgroundColor: 'rgb(241, 241, 239)',
+  },
+  brown: {
+    color: 'rgb(159, 107, 83)',
+    border: '1px solid rgba(55, 53, 47, 0.16)',
+  },
+  brown_background: {
+    backgroundColor: 'rgb(244, 238, 238)',
+  },
+  orange: {
+    color: 'rgb(217, 115, 13)',
+    border: '1px solid rgba(55, 53, 47, 0.16)',
+  },
+  orange_background: {
+    backgroundColor: 'rgb(251, 236, 221)',
+  },
+  yellow: {
+    color: 'rgb(203, 145, 47)',
+    border: '1px solid rgba(55, 53, 47, 0.16)',
+  },
+  yellow_background: {
+    backgroundColor: 'rgb(251, 243, 219)',
+  },
+  green: {
+    color: 'rgb(68, 131, 97)',
+    border: '1px solid rgba(55, 53, 47, 0.16)',
+    backgroundColor: 'inherit',
+  },
+  green_background: {
+    backgroundColor: 'rgb(237, 243, 236)',
+  },
+  blue: {
+    color: 'rgb(51, 126, 169)',
+    border: '1px solid rgba(55, 53, 47, 0.16)',
+  },
+  blue_background: {
+    backgroundColor: 'rgb(231, 243, 248)',
+  },
+  purple: {
+    color: 'rgb(144, 101, 176)',
+    border: '1px solid rgba(55, 53, 47, 0.16)',
+  },
+  purple_background: {
+    backgroundColor: 'rgba(244, 240, 247, 0.8)',
+  },
+  pink: {
+    color: 'rgb(193, 76, 138)',
+    border: '1px solid rgba(55, 53, 47, 0.16)',
+  },
+  pink_background: {
+    backgroundColor: 'rgba(249, 238, 243, 0.8)',
+  },
+  red: {
+    color: 'rgb(212, 76, 71)',
+    border: '1px solid rgba(55, 53, 47, 0.16)',
+  },
+  red_background: {
+    backgroundColor: 'rgb(253, 235, 236)',
   },
 })
 
 const Icon = ({ block }: CalloutBlockProps) => {
-  if (block.callout.icon === null) {
+  if (!block.callout.icon) {
     return <></>
   }
 
@@ -42,12 +114,12 @@ const Icon = ({ block }: CalloutBlockProps) => {
 }
 
 const CalloutBlock = ({ block }: CalloutBlockProps) => {
-  const { rich_text } = block.callout
+  const { color, rich_text } = block.callout
 
   return (
-    <div className={`rotion-callout ${Stylex(style.callout)}`}>
+    <div className={`rotion-callout rotion-callout-color-${color} ${Stylex(style.wrapper)} ${Stylex(style[color])}`}>
       <div className={`rotion-callout-icon ${Stylex(style.icon)}`}>
-        {<Icon block={block} />}
+        <Icon block={block} />
       </div>
       <div className={`rotion-callout-text ${Stylex(style.text)}`}>
         {rich_text.map((v, i) => (
