eb989fcac761b7b22389ecb382813017e932fab7
linyows <linyows@gmail.com>
2024-02-09 21:15:27 +0900

    support LinkPreviewMention

--- src/ui/components/Page/RichText/Mention/LinkPreviewMention/LinkPreviewMention.tsx
+++ src/ui/components/Page/RichText/Mention/LinkPreviewMention/LinkPreviewMention.tsx
@@ -2,16 +2,64 @@ import React from 'react'
 import { pathBasename } from '../../../../lib'
 import PageIcon from '../../../PageIcon/PageIcon'
 import type { LinkPreviewMemtionProps } from './LinkPreviewMention.types'
+import Stylex from '@stylexjs/stylex'
+import { tokens, link } from '../../../../tokens.stylex'
 
-const LinkPreviewMention = ({ url, text, children }: LinkPreviewMemtionProps) => {
+const styles = Stylex.create({
+  wrapper: {
+    fontFamily: tokens.fontFamily,
+    color: link.color,
+    backgroundColor: {
+      default: link.backgroundColor,
+      ':hover': link.backgroundColorHover,
+    },
+    padding: '.1rem .2rem',
+    borderRadius: tokens.borderRadius,
+    lineHeight: 1,
+    textDecoration: link.textDecoration,
+  },
+  icon: {
+    verticalAlign: 'middle',
+    paddingRight: '.3rem',
+  },
+  text: {
+    borderBottom: {
+      default: link.borderBottom,
+      ':hover': link.borderBottomHover,
+    },
+  }
+})
+
+const LinkPreviewMention = ({ url, text }: LinkPreviewMemtionProps) => {
+  if (url.includes('slack.com')) {
+    return (
+      <a className={`rotion-mention-linkpreview ${Stylex(styles.wrapper)}`} href={url} rel="noreferrer" target="_blank">
+        <PageIcon name='slack' className={Stylex(styles.icon)} width='17px' height='17px' />
+        <span className={`rotion-mention-linkprevew-text ${Stylex(styles.text)}`}>
+          Message in Slack
+        </span>
+      </a>
+    )
+  }
+
+  if (url.includes('figma.com')) {
+    return (
+      <a className={`rotion-mention-linkpreview ${Stylex(styles.wrapper)}`} href={url} rel="noreferrer" target="_blank">
+        <PageIcon name='figma' className={Stylex(styles.icon)} width='10px' height='16px' />
+        <span className={`rotion-mention-linkprevew-text ${Stylex(styles.text)}`}>
+          {pathBasename(text).replace(/-/g, ' ')}
+        </span>
+      </a>
+    )
+  }
+
   return (
-    <span className={'rotion-mention-linkpreview'}>
-      {url.includes('github.com') && <PageIcon name='github' />}
-      {url.includes('slack.com') && <PageIcon name='slack' />}
-      {url.includes('figma.com') && <PageIcon name='figma' />}
-      {pathBasename(text)}
-      {children}
-    </span>
+    <a className={`rotion-mention-linkpreview ${Stylex(styles.wrapper)}`} href={url} rel="noreferrer" target="_blank">
+      {url.includes('github.com') && <PageIcon name='github' className={Stylex(styles.icon)} width='17px' height='17px' />}
+      <span className={`rotion-mention-linkprevew-text ${Stylex(styles.text)}`}>
+        {pathBasename(text)}
+      </span>
+    </a>
   )
 }
 
