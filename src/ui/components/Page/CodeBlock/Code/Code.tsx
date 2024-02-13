1dae02d97589687763785983920715c89c950cd9
linyows <linyows@gmail.com>
2024-02-13 10:47:43 +0900

    bump version to 0.0.14

--- src/ui/components/Page/CodeBlock/Code/Code.tsx
+++ src/ui/components/Page/CodeBlock/Code/Code.tsx
@@ -3,11 +3,6 @@ import type { CodeProps } from './Code.types'
 import Stylex from '@stylexjs/stylex'
 import mermaid from 'mermaid'
 import Prism from 'prismjs'
-import 'prismjs/themes/prism.min.css'
-import 'prismjs/plugins/autoloader/prism-autoloader'
-if (Prism.plugins.autoloader) {
-  Prism.plugins.autoloader.languages_path = 'https://unpkg.com/prismjs@1.29.0/components/'
-}
 
 const style = Stylex.create({
   wrapper: {
