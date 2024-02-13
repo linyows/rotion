65ed388aa7f5f3da3b4aa7ef77763a229ceeb21b
linyows <linyows@gmail.com>
2024-02-13 10:49:45 +0900

    remove rollup-plugin-import-css

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,6 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import del from 'rollup-plugin-delete'
 import dts from 'rollup-plugin-dts'
 import stylexPlugin from '@stylexjs/rollup-plugin'
-import css from 'rollup-plugin-import-css'
 
 export default [
   {
@@ -40,7 +39,6 @@ export default [
       del({ targets: './dist/ui/*' }),
       peerDepsExternal(),
       resolve(),
-      css(),
       json(),
       commonjs(),
       typescript(),
