e33b9d41017b5169962a737984921dd335b2f04c
linyows <linyows@gmail.com>
2024-03-28 23:45:18 +0900

    update storybook to v8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,6 +11,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@chromatic-com/storybook"
   ],
   framework: {
     name: "@storybook/react-vite",
