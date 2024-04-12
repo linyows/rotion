f11e1bd6641c548edee2caa4590e5f9313355450
linyows <linyows@gmail.com>
2024-04-12 21:28:51 +0900

    remove unused var

--- src/ui/components/lib.ts
+++ src/ui/components/lib.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import type { ParsedUrlQueryInput } from 'node:querystring'
 import type { ListBlockChildrenResponseEx } from '../../exporter'
 
