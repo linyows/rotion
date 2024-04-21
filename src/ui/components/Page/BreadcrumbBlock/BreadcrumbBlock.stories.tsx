8c7cacf4ec7195d84b2c43e94edf5388f590d4cf
linyows <linyows@gmail.com>
2024-04-21 23:37:28 +0900

    use pure-css instead of stylex for BreadcrumbBlock

--- src/ui/components/Page/BreadcrumbBlock/BreadcrumbBlock.stories.tsx
+++ src/ui/components/Page/BreadcrumbBlock/BreadcrumbBlock.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import type { BreadcrumbBlockObjectResponseEx } from '../../../../exporter'
 import BreadcrumbBlock from './BreadcrumbBlock'
+import './BreadcrumbBlock.css'
 
 const block: BreadcrumbBlockObjectResponseEx = {
   object: 'block',
--- src/ui/components/Page/BreadcrumbBlock/Breadcrumbs.tsx
+++ src/ui/components/Page/BreadcrumbBlock/Breadcrumbs.tsx
@@ -2,47 +2,22 @@ import React from 'react'
 import LinkedBreadcrumbIfLinked from './LinkedBreadcrumbIfLinked'
 import type { Breadcrumb } from '../../../../exporter'
 import type { BreadcrumbsProps } from './Breadcrumbs.types'
-import Stylex from '@stylexjs/stylex'
-import { tokens } from '../../tokens.stylex'
-
-const style = Stylex.create({
-  wrapper: {
-    fontFamily: tokens.fontFamily,
-  },
-  icon: {
-    verticalAlign: 'middle',
-    paddingRight: '.4rem',
-    paddingBottom: '.2rem',
-  },
-  emoji: {
-    verticalAlign: 'middle',
-    paddingRight: '.4rem',
-    paddingBottom: '.25rem',
-  },
-  slash: {
-    fontSize: '.9rem',
-    opacity: '.5',
-    padding: '0 .3rem',
-    color: tokens.secondaryText,
-  },
-  title: {},
-})
 
 // Note: This omponent cut out from BreadcrumbBlock. To call the breadcrumb list from other than Page. So, Don't integrate.
 const Breadcrumbs = ({ list, link, hrefs, query }: BreadcrumbsProps) => {
   const max = list.length
   return (
-    <div className={`rotion-breadcrumb ${Stylex(style.wrapper)}`}>
+    <div className="rotion-breadcrumb">
       {list.map((v: Breadcrumb, i: number) => (
         <span key={`crumb-${i}`}>
           <LinkedBreadcrumbIfLinked breadcrumb={v} href={hrefs === undefined ? undefined : hrefs[i]} link={link} query={query}>
-            {v.icon.type === 'emoji' && <span className={`rotion-breadcrumb-emoji ${Stylex(style.emoji)}`}>{v.icon.emoji}</span>}
-            {v.icon.type !== 'emoji' && <img className={`rotion-breadcrumb-icon ${Stylex(style.icon)}`} src={v.icon.src} width={20} height={20} alt={v.name} />}
-            <span className={`rotion-breadcrumb-title ${Stylex(style.title)}`}>
+            {v.icon.type === 'emoji' && <span className="rotion-breadcrumb-emoji">{v.icon.emoji}</span>}
+            {v.icon.type !== 'emoji' && <img className="rotion-breadcrumb-icon" src={v.icon.src} width={20} height={20} alt={v.name} />}
+            <span className="rotion-breadcrumb-title">
               {v.name}
             </span>
           </LinkedBreadcrumbIfLinked>
-          {i + 1 < max && <span className={`rotion-breadcrumb-slash ${Stylex(style.slash)}`}>/</span>}
+          {i + 1 < max && <span className="rotion-breadcrumb-slash">/</span>}
         </span>
       ))}
     </div>
