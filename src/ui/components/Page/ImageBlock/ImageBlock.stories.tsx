add94797f425b8a6d583f15b3634fbe43bfad54c
linyows <linyows@gmail.com>
2024-04-24 18:31:37 +0900

    use pure-css instead of stylex for ImageBlock

--- src/ui/components/Page/ImageBlock/ImageBlock.stories.tsx
+++ src/ui/components/Page/ImageBlock/ImageBlock.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import type { ImageBlockObjectResponseEx } from '../../../../exporter'
 import ImageBlock from './ImageBlock'
+import './ImageBlock.css'
 
 const block: ImageBlockObjectResponseEx = {
   object: 'block',
