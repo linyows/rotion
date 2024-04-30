64e8cbbda85ae16bc5eac2f69a59adab282153e1
linyows <linyows@gmail.com>
2024-04-30 11:14:01 +0900

    fix breadcrumbs

--- src/ui/components/Page/BreadcrumbBlock/BreadcrumbBlock.stories.tsx
+++ src/ui/components/Page/BreadcrumbBlock/BreadcrumbBlock.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import type { BreadcrumbBlockObjectResponseEx } from '../../../../exporter'
 import BreadcrumbBlock from './BreadcrumbBlock'
 import './BreadcrumbBlock.css'
+import './LinkedBreadcrumbIfLinked.css'
 
 const block: BreadcrumbBlockObjectResponseEx = {
   object: 'block',
