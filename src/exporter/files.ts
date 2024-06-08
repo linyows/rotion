47712c53f364ffa0b0b71d80c829bff55853351f
linyows <linyows@gmail.com>
2024-06-08 22:12:20 +0900

    add x.com

--- src/exporter/files.ts
+++ src/exporter/files.ts
@@ -453,7 +453,7 @@ export const getEmbedHtml = async (block: EmbedBlockObjectResponseEx): Promise<s
     // Instagram oEmbed API is authentication required. So hard-coding.
     return `<blockquote class="instagram-media" data-instgrm-permalink="${link}?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:540px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);"> </blockquote> <script async src="//www.instagram.com/embed.js"></script>`
 
-  } else if (block.embed && block.embed.url.includes('twitter.com')) {
+  } else if (block.embed && (block.embed.url.includes('x.com') || block.embed.url.includes('twitter.com'))) {
     const src = block.embed?.url || ''
     const tweetId = path.basename(src.split('?').shift() || '')
     const reqUrl = `https://api.twitter.com/1/statuses/oembed.json?id=${tweetId}`
