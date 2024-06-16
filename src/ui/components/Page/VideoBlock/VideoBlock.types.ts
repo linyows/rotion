1aaaaf5b7cdbcb5b8ac6d00fb6c7002175f51be0
linyows <linyows@gmail.com>
2024-06-16 12:06:05 +0900

    add types to VideoBlock.types.ts

--- src/ui/components/Page/VideoBlock/VideoBlock.types.ts
+++ src/ui/components/Page/VideoBlock/VideoBlock.types.ts
@@ -1,5 +1,17 @@
-import type { VideoBlockObjectResponseEx } from '../../../../exporter'
+import type { VideoBlockObjectResponseEx, VideoFile, VideoExternal, RichTextItemResponse } from '../../../../exporter'
 
 export interface VideoBlockProps {
   block: VideoBlockObjectResponseEx
+}
+
+export interface VideoFileProps {
+  video: VideoFile
+}
+
+export interface VideoExternalProps {
+  video: VideoExternal
+}
+
+export interface CaptionProps {
+  caption: RichTextItemResponse[]
 }
