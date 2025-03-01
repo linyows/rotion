573f7cb9dca5697ce3cab051350c597e5837c3b9
linyows <linyows@gmail.com>
2025-03-01 22:47:05 +0900

    add class

--- src/ui/components/Page/ImageBlock/ImageBlock.tsx
+++ src/ui/components/Page/ImageBlock/ImageBlock.tsx
@@ -5,10 +5,16 @@ import '../../tokens.css'
 import './ImageBlock.css'
 
 export const ImageBlock = ({ block }: ImageBlockProps) => {
+  let cname = 'rotion-image-ratio-unknown'
+  if (block.image && block.image.width && block.image.height) {
+    const { width, height } = block.image
+    cname = width > height ? 'rotion-image-ratio-landscape' : 'rotion-image-ratio-portrait'
+  }
+
   return (
     <div className="rotion-image">
       <div className="rotion-image-area">
-        <img className="rotion-image-img" src={block.image?.src} alt="Image" />
+        <img className={`rotion-image-img ${cname}`} src={block.image?.src} alt="Image" />
       </div>
       <div className="rotion-image-caption">
         {block.image.caption.map((v, i) => (
